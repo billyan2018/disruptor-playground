210f43886f504deea43a8d8a92f992c5466fe75a
Bill yan <billyan2018@icloud.com>
2024-11-06 02:39:03 +0000

    format

--- src/main.rs
+++ src/main.rs
@@ -44,30 +44,31 @@ fn main() {
     };
 
     let mut producer1 = disruptor::build_multi_producer(64, factory, BusySpin)
-        .pin_at_core(1).handle_events_with(h1)
-        .pin_at_core(2).handle_events_with(h2)
+        .pin_at_core(2)
+        .handle_events_with(h1)
+        .pin_at_core(3)
+        .handle_events_with(h2)
         .and_then()
-        .pin_at_core(3).handle_events_with(h3)
+        .pin_at_core(4)
+        .handle_events_with(h3)
         .build();
 
     let mut producer2 = producer1.clone();
 
-    let handle1 = thread::spawn(move || {
-        for i in 0..10 {
-            producer1.publish(|e| {
-                e.price = i as f64;
-            });
-        }
+    thread::scope(|s| {
+        s.spawn(move || {
+            for i in 0..10 {
+                producer1.publish(|e| {
+                    e.price = i as f64;
+                });
+            }
+        });
+        s.spawn(move || {
+            for i in 10..20 {
+                producer2.publish(|e| {
+                    e.price = i as f64;
+                });
+            }
+        });
     });
-
-    let handle2 = thread::spawn(move || {
-        for i in 10..20 {
-            producer2.publish(|e| {
-                e.price = i as f64;
-            });
-        }
-    });
-
-    handle1.join().unwrap();
-    handle2.join().unwrap();
 }
